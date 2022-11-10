24d094bfb320f55c9be617e1f3975f24cd8c2386
Smrity44 <Smritybdny@gmail.com>
2022-11-10 13:52:28 -0500

    Adding javaQAE to git

---
+++ src/Alexa.java
@@ -0,0 +1,17 @@
+public class Alexa {
+
+    public Alexa(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+    public void turnOnFan(){
+        System.out.println("Turn on Fan");
+    }
+
+    public void turnOffFan(){
+        System.out.println("Turn off Fan");
+    }
+}
